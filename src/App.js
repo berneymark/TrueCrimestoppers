7adf7e0fbe576c98d2c81cd031de8d1eccd2ae8e
Mark Berney <berneydwmark@gmail.com>
2022-11-10 15:30:29 +1300

    added react router to navbar pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
+// Library Imports
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+
+// Page Imports
+import About from "./pages/About.js";
+import Contact from "./pages/Contact.js";
 import Home from "./pages/Home.js";
+
+// Component Imports
 import Navbar from "./components/Navbar.js"
 
 import "./App.css";
@@ -7,7 +19,19 @@ function App() {
   return (
     <div>
       <Navbar/>
-      <Home/>
+      <Router>
+        <Routes>
+          <Route 
+            path="/"
+            element={<Home/>}/>
+            <Route 
+              path="/about"
+              element={<About/>}/>
+          <Route 
+            path="/contact"
+            element={<Contact/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
