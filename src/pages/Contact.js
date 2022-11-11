2cc824d2277e4a1aa09ffc3a0bcd93cd108daa18
Mark Berney <berneydwmark@gmail.com>
2022-11-11 17:44:25 +1300

    basic contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,8 +3,22 @@ import React from "react";
 export default class Contact extends React.Component {
   render() {
     return (
-      <main>
-        <p>Contact</p>
+      <main id="contact-page">
+        <h1>Contact</h1>
+        <form id="contact-form">
+          <input
+            type="name"
+            placeholder="Enter your name..."/>
+          <input
+            type="email"
+            placeholder="Enter your email..."/>
+          <input
+            type="text"
+            placeholder="Enter your message..."/>
+          <input
+            type="submit"
+            value="Submit"/>
+        </form>
       </main>
     );
   }
