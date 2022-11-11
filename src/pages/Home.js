993d16c1e1b7ec633906afcc0efca7a0f508c744
Mark Berney <berneydwmark@gmail.com>
2022-11-11 17:35:58 +1300

    changed home page title

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ export default class Home extends React.Component {
   render() {
     return (
       <main id="home-page">
-        <h1>Home</h1>
+        <h1>The Responsibility Contract</h1>
       </main>
     );
   }
