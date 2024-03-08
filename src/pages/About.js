2cf2b2531049d46024111ad1fa92368b641c059b
Mark Berney <berneydwmark@gmail.com>
2024-03-08 15:25:13 +1300

    updated code readability for App.css file

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,13 +4,13 @@ export default class About extends React.Component {
   render() {
     return (
       <main id="about-page">
-        <section id="heading">
+        <section id="about-hero">
           <h1>Our Mission</h1>
           <p>
             Our Mission is to truly prevent crime before it can hurt anyone.
           </p>
         </section>
-        <section id="sections">
+        <section id="about-features">
           <div>
             <div class="about-circle"></div>
             <h1>Our Founder</h1>
