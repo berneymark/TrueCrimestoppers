02aff9831736fd51ffcedb909d68c3847c94c875
Mark Berney <berneydwmark@gmail.com>
2024-03-08 15:32:52 +1300

    updated contact page with phone number and email space

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,6 +5,12 @@ export default class Contact extends React.Component {
     return (
       <main id="contact-page">
         <h1>Contact</h1>
+        <p>You can contact us by any of the below means.</p>
+        <p>You can also send us a message in the below message box.</p>
+        <div id="contact-details">
+          <p>Phone Number: 027 1234 5678</p>
+          <p>Email: truecrimestoppers@fakeemail.com</p>
+        </div>
         <form id="contact-form">
           <input
             type="name"
