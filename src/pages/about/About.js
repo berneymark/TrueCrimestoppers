be503191d9457ff5852ef0789ede76326d009989
Mark Berney <berneydwmark@gmail.com>
2024-03-14 19:23:07 +1300

    updated mission statement

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -7,7 +7,9 @@ export default class About extends React.Component {
         <section id="about-hero">
           <h1>Our Mission</h1>
           <p>
-            Our Mission is to truly prevent crime before it can hurt anyone.
+            Empowering communities to build a safer future by bringing about a
+            world <br /> where we take responsibility for our actions, and how
+            they affect those around us.
           </p>
         </section>
         <section id="about-features">
