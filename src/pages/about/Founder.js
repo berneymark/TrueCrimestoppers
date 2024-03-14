09e67eaece1ff9a68e9b636c6d11c74710644a3f
Mark Berney <berneydwmark@gmail.com>
2024-03-15 12:04:39 +1300

    updated founder quotes

--- src/pages/about/Founder.js
+++ src/pages/about/Founder.js
@@ -7,7 +7,7 @@ export default class Founder extends React.Component {
     return (
       <main id="our-founder">
         <h1>Our Founder</h1>
-        <section id="founder-content">
+        <section id="founder-about">
           <section id="founder-text">
             <p>
               The founder of True Crimestoppers, Kevin Boyle, was a policeman in
@@ -30,6 +30,23 @@ export default class Founder extends React.Component {
             src={PlaceholderAvatar}
             alt="Placeholder of Kevin"
           />
+        </section>
+        <section id="founder-quotes">
+          <p>
+            "As an ex-policeman who also worked professionally with the policing
+            ideology in private security management in New Zealand commerce, I
+            am proud to have served in both roles. But having the opportunity to
+            work alongside people in their workplace gave me greater insight
+            into the inability of the security management’s role too prevent the
+            internal offending it was employed to prevent. There was no doubt it
+            was successful in investigating and prosecuting offending employees,
+            which was unfortunately the measure of the role’s success,
+            justifying its presence and ongoing need, but it did not prevent
+            offending and indeed aggravated it. This lead to my research with
+            employees and management which founded The Responsibility Contract
+            that prevents offending in the antithesis of the policing ideology."
+          </p>
+          <p>- Kevin Boyle</p>
         </section>
       </main>
     );
