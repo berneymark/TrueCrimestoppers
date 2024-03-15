b24af03d08bfb1ded6e9f94a6f0f0a9b332f0b27
Mark Berney <berneydwmark@gmail.com>
2024-03-16 11:19:01 +1300

    replace about page text from Kevin

--- src/pages/about/Founder.js
+++ src/pages/about/Founder.js
@@ -10,10 +10,113 @@ export default class Founder extends React.Component {
         <section id="founder-about">
           <section id="founder-text">
             <p>
-              The founder of True Crimestoppers, Kevin Boyle, was a policeman in New Zealand in the 1970s and a security advisor to New Zealand public companies throughout the 1980s-90s.
+              In the 1990's, I questioned what I believed to be a faulted international best practice to prevent theft and fraud in the workplace, to establish The Responsibility Contract that prevented previous $multi-million losses and symptomatic job redundancies, before the best international practice was dogmatically reinstated by the established order and the losses returned. 
             </p>
             <p>
-              As a security manager, he learned that the policing ideology transferred into the proprietary management role did not prevent repetitive internal offending and appeared to exacerbate it. Kevin’s research in the field founded The Responsibility Contract, a social contract needed to overcome the obstructive criminal code of silence caused by the policing ideology, which opened the floodgates to enabling unprecedented intervention and prevention outcomes, saving previous losses totalling multi-millions.
+              I stood by my newly found crime prevention solution, over my chosen professions in policing and private security management, eventually being forced out by those with dogmatic institutionalized adherence to the policing ideology that underpins security management. It was an irresistible force against a lone voice. 
+            </p>
+            <p>
+              I accepted that this was as much due to fear of change as it was due to self-interest in protecting a preeminent commercial position in that quickly growing industry built on fear. 
+            </p>
+            <p>
+              I continued my promotion of The Responsibility Contract from outside these realms into my father’s profession as a house painter in the building industry. 
+            </p>
+            <p>
+              With little responsibility and plenty of time to reflect and continue important research and development of The Responsibility Contract it gave me the flexibility to leave at short notice to pioneer it as a new innovative remedy to what continues to this day to remain a failed international crime prevention method. (Read more)
+            </p>
+            <p>
+              From the planks and ladders of house painting, I was able to promote it as a new innovative, successful,  alternative crime and corruption method to many domestic and international parties and organizations where it was unanimously  approved but unable to attract a “first to get their feet wet” pioneer of a new innovative idea to challenge policing, one of society’s longest standing and most trusted institutions.
+            </p>
+            <p>
+              I understood this as it represented a societal change in attitude but I equally understood that the fault incurred humungous unnecessary fiscal and social costs to society. Inevitably, the unanimous approbation  promised to adopt The Responsibility Contract once I had it “up and running”. 
+            </p>
+            <p>
+              Here is a sample of those I proposed it to and received a promise of later support. 
+            </p>
+            <h2>
+              INTERNATIONAL CRICKET COUNCIL (ICC)
+            </h2>
+            <p>
+              In 2000 when Hanse Cronje’s match fixing corruption scandal surfaced in international cricket, I anticipated the policing ideology in security management in the form of a “super cop” would be appointed. I knew this would be a mistake and proposed The Responsibility Contract to the ICC as an alternative remedy.  
+            </p>
+            <p>
+              Proprietary security management had not yet entered the sporting arena and I anticipated this would portend the direction of all international sports that were equally experiencing corruption events. I knew it would be a long shot but was determined to have my alternative solution considered from the outset. 
+            </p>
+            <p>
+              I was gratified that my proposal was well received but understood the global  furore surrounding the events that demanded a strong punitive response that the policing ideology provided.
+            </p>
+            <p>
+              Although I didn’t believe this would eventuate, as was subsequently proven, my idea was proposed as an ongoing new prevention management method; but it could not head off the already established “super cop” method.  
+            </p>
+            <p>
+              Notwithstanding, the New Zealand Cricket(NZC)! CEO, Christopher Doig, who presented the proposal to the ICC, wanted the New Zealand team to adopt my solution and to later repropose it to the ICC should my predictions later prove to be correct (which they were).
+            </p>
+            <p>
+              Unfortunately, he was not supported by the NZ captain who instead supported the team’s psychological advisor who had established his own corruption prevention protocols within the team that unfortunately did not prevent his players from being involved in future match fixing.
+            </p>
+            <p>
+              The Responsibility Contract remains proposed to the ICC and NZC to better prevent match fixing and all other corruptive threats. 
+            </p>
+            <p>
+              Following crickets lead, other international sports, including tennis and racing,  immediately adopted the policing ideology in private  security management to deal with and prevent ongoing corruption threats.
+            </p>
+            <p>
+              I continued proposing The Responsibility Contract to these and other sporting entities suffering from well documented corruption problems, including…
+            </p>
+            <h2>
+              WORLD ANTI-DOPING AGENCY (WADA)
+            </h2>
+            <p>
+              Continues to use policing’s failed enforcement ideology to manage and prevent athletes using performance enhancing drugs and those contining to manufacture new performance enhancing drugs and masking agents.
+            </p>
+            <h2>
+              FEDERATION INTERNATIONALE de FOOTBALL ASSOCIATION (FIFA)
+            </h2>
+            <p>
+              Well documented historical corruption threats continue to be managed by failed prevention measures.
+            </p>
+            <h2>
+              INTERNATIONAL OLYMPIC  COMMITTEE (IOC)
+            </h2>
+            <p>
+              Continues to rely on same failed best practice to catch the cheats and prevent their threat and impact on Olympic sports.
+            </p>
+            <h2>
+              INTERNATIONAL AMATUER ATHLETIC FEDERATION (IAAF)
+            </h2>
+            <p>
+              Continues to rely on failed internal security management to catch the cheats and prevent corruption.
+            </p>
+            <p>
+              All of the above organizations have been proposed The Responsibility Contract to better manage and prevent their corruption threats and all have received  it well and indicated support when the new innovation is “up and running” as an accepted best practice.
+            </p>
+            <p>
+              Apart from sport, I have proposed The Responsibility Contract to a number of other organizations and commercial entities using the existing failed best practice to prevent internal offending and corruption with similar outcomes.
+            </p>
+            <h2>
+              Me Too
+            </h2>
+            <p>
+              This includes the Me Too movement that is relatively newly-formed to combat sexual harassment in the workplace.
+            </p>
+            <p>
+              The Harvey Weinstein’s or Donald Trump’s of this world would not prevail under The Responsibility Contract, whereas the ongoing threat continues to be overseen by failed existing best practices.
+            </p>
+            <h2>In Closing...</h2>
+            <p>
+              I understand the dilemma and difficulty of change, particularly a complete societal reversal of long accepted conventions that stem from a previously unknown fault in one of society’s most longstanding, trusted institutions, policing.
+            </p>
+            <p>
+              But the fact remains that the fault exists and will no go away by ignoring it.
+            </p>
+            <p>
+              I founded The Responsibility Contract as its only remedy, there may another that I am unaware of. However, in the absence another solution, I would now propose The Responsibility Contract to society at large for public discussion and deliberation, to reinforce its efficacy to the leaders of the entities and organizations above who represent you and who are awaiting your direction on such social change.
+            </p>
+            <p>
+              For me the choice is simple.
+            </p>
+            <p>
+              Continue to tolerate crime and corruption, incurring huge unnecessary fiscal and social costs, simply because we cannot prevent it. Or adopt change that can prevent crime and corruption to save the debilitating fiscal and social costs to society.
             </p>
           </section>
           <img
@@ -21,21 +124,6 @@ export default class Founder extends React.Component {
             src={PlaceholderAvatar}
             alt="Placeholder of Kevin"
           />
-        </section>
-        <section id="quotes">
-          <blockquote>
-            As an ex-policeman who also worked professionally with the policing ideology in private security management in New Zealand commerce, I am proud to have served in both roles. But having the opportunity to work alongside people in their workplace gave me greater insight into the inability of the security management’s role too prevent the internal offending it was employed to prevent. There was no doubt it was successful in investigating and prosecuting offending employees, which was unfortunately the measure of the role’s success, justifying its presence and ongoing need, but it did not prevent offending and indeed aggravated it. This lead to my research with employees and management which founded The Responsibility Contract that prevents offending in the antithesis of the policing ideology.
-            <br />
-            <br />
-            Peers were uncomfortable with my research findings, preferring to celebrate the success of prosecution outcomes that justified their ongoing roles. This unfortunately transferred into actively opposing and trying to discredit me, which exposed another fault in policing having sole stewardship of societal crime prevention method and advice, allowing it to control the message around committed crime, obfuscating its failure to prevent and obstructing it being questioned or challenged with new innovation.
-            <br />
-            <br />
-            I believe policing is unwitting of the reasons for its failure in crime prevention which is why public scrutiny and discussion is needed about the merits of my fundings and conclusions that moved regular people who were victims of the fault to found{" "}
-            <a href="/about/the-responsibility-contract">
-              The Responsibility Contract.
-            </a>
-            <p>- Kevin Boyle</p>
-          </blockquote>
         </section>
       </main>
     );
