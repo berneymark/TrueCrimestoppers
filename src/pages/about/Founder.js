e99a8bba1b682cd6e9a073c1ea6b9fa9dceff4a4
Mark Berney <berneydwmark@gmail.com>
2024-04-28 17:16:00 +1200

    founder page additions

--- src/pages/about/Founder.js
+++ src/pages/about/Founder.js
@@ -33,75 +33,7 @@ export default class Founder extends React.Component {
             <p>
               Here is a sample of those I proposed it to and received a promise of later support. 
             </p>
-            <h2>
-              INTERNATIONAL CRICKET COUNCIL (ICC)
-            </h2>
-            <p>
-              In 2000 when Hanse Cronje’s match fixing corruption scandal surfaced in international cricket, I anticipated the policing ideology in security management in the form of a “super cop” would be appointed. I knew this would be a mistake and proposed The Responsibility Contract to the ICC as an alternative remedy.  
-            </p>
-            <p>
-              Proprietary security management had not yet entered the sporting arena and I anticipated this would portend the direction of all international sports that were equally experiencing corruption events. I knew it would be a long shot but was determined to have my alternative solution considered from the outset. 
-            </p>
-            <p>
-              I was gratified that my proposal was well received but understood the global  furore surrounding the events that demanded a strong punitive response that the policing ideology provided.
-            </p>
-            <p>
-              Although I didn’t believe this would eventuate, as was subsequently proven, my idea was proposed as an ongoing new prevention management method; but it could not head off the already established “super cop” method.  
-            </p>
-            <p>
-              Notwithstanding, the New Zealand Cricket(NZC)! CEO, Christopher Doig, who presented the proposal to the ICC, wanted the New Zealand team to adopt my solution and to later repropose it to the ICC should my predictions later prove to be correct (which they were).
-            </p>
-            <p>
-              Unfortunately, he was not supported by the NZ captain who instead supported the team’s psychological advisor who had established his own corruption prevention protocols within the team that unfortunately did not prevent his players from being involved in future match fixing.
-            </p>
-            <p>
-              The Responsibility Contract remains proposed to the ICC and NZC to better prevent match fixing and all other corruptive threats. 
-            </p>
-            <p>
-              Following crickets lead, other international sports, including tennis and racing,  immediately adopted the policing ideology in private  security management to deal with and prevent ongoing corruption threats.
-            </p>
-            <p>
-              I continued proposing The Responsibility Contract to these and other sporting entities suffering from well documented corruption problems, including…
-            </p>
-            <h2>
-              WORLD ANTI-DOPING AGENCY (WADA)
-            </h2>
-            <p>
-              Continues to use policing’s failed enforcement ideology to manage and prevent athletes using performance enhancing drugs and those contining to manufacture new performance enhancing drugs and masking agents.
-            </p>
-            <h2>
-              FEDERATION INTERNATIONALE de FOOTBALL ASSOCIATION (FIFA)
-            </h2>
-            <p>
-              Well documented historical corruption threats continue to be managed by failed prevention measures.
-            </p>
-            <h2>
-              INTERNATIONAL OLYMPIC  COMMITTEE (IOC)
-            </h2>
-            <p>
-              Continues to rely on same failed best practice to catch the cheats and prevent their threat and impact on Olympic sports.
-            </p>
-            <h2>
-              INTERNATIONAL AMATUER ATHLETIC FEDERATION (IAAF)
-            </h2>
-            <p>
-              Continues to rely on failed internal security management to catch the cheats and prevent corruption.
-            </p>
-            <p>
-              All of the above organizations have been proposed The Responsibility Contract to better manage and prevent their corruption threats and all have received  it well and indicated support when the new innovation is “up and running” as an accepted best practice.
-            </p>
-            <p>
-              Apart from sport, I have proposed The Responsibility Contract to a number of other organizations and commercial entities using the existing failed best practice to prevent internal offending and corruption with similar outcomes.
-            </p>
-            <h2>
-              Me Too
-            </h2>
-            <p>
-              This includes the Me Too movement that is relatively newly-formed to combat sexual harassment in the workplace.
-            </p>
-            <p>
-              The Harvey Weinstein’s or Donald Trump’s of this world would not prevail under The Responsibility Contract, whereas the ongoing threat continues to be overseen by failed existing best practices.
-            </p>
+            
             <h2>In Closing...</h2>
             <p>
               I understand the dilemma and difficulty of change, particularly a complete societal reversal of long accepted conventions that stem from a previously unknown fault in one of society’s most longstanding, trusted institutions, policing.
