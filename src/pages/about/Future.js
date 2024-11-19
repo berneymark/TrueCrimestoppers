8bdf43a4b890c75bdd91083d7d8cea5b628874f2
Mark Berney <berneydwmark@gmail.com>
2024-11-19 15:32:43 +1300

    fixed issue where text unexpectedly centered

--- src/pages/about/Future.js
+++ src/pages/about/Future.js
@@ -4,79 +4,75 @@ export default class Future extends React.Component {
   render() {
     return (
       <main class="article" id="our-idea">
-        <h1>Our Goals</h1>
-        <p>
-          TRUE CRIMESTOPPERS’ first goal is to prevent crime but it recently took on an overarching goal to strengthen guardrails around democracy. 
-        </p>
-        <p>
-          Donald Trump’s victory in the 2024 American election saw his autocratic threat re-emerge which follows extremist trends globally that pose similar threats to democracies. 
-        </p>
-        <p>
-          TRUE CRIMESTOPPERS is non partisan and supports the rule of law underpinning democratic societies to maintain social order and keep people safe. 
-        </p>
-        <p>
-          Donald Trump’s lifetime of moral turpitude has seen him regularly breach the rule of law and defeat all democratic guardrails to be rewarded with the most powerful position in the free world to destroy it for everyone else. 
-        </p>
-        <p>
-          He did not achieve this through political prowess, but by taking advantage of a critical hidden fault in the existing guardrails that has never before been exposed or considered. 
-        </p>
-        <p>
-          TRUE CRIMESTOPPERS exposes this fault in its remedy to Donald Trump’s offending and avoidance of accountability that the existing guardrails could not achieve. 
-        </p>
-        <p>
-          And it also provides the impetus to prevent crime and corruption for the first time in the history of democratic governance. 
-        </p>
-        <p>
-          Ironic, that his playbook to offend and get away with it could now be used to make him accountable and to prevent him and his kind from becoming a demagogue the founding fathers forewarned but could not prevent.  
-        </p>
-        <p>
-          It is also ironic that his final extreme threat to democracy needed to be played out in the presidency in order for TRUE CRIMESTOPPERS’ remedy to be recognized and considered. 
-        </p>
-        <p>
-          Its remedy doesn’t take account of economic, policy or  political reasons Donald Trump was re-elected but focuses instead on a fault in policing and the criminal justice system that is unwitting. 
-        </p>
-        <p>
-          A fault that explains his base and why it supports him in spite of his criminal convictions and obvious moral turpitude. And it explains why that support attracted more momentum with increasing prosecution of his malfeasance. 
-        </p>
-        <p>
-          It explains why Donald Trump became a cult hero, not a cult leader because there was no discernible cult. But there was a legitimate cause in the fault that is one of its consequences. 
-        </p>
-        <p>
-          Whilst it legitimized his base it did not legitimize the fear and intimidatory threat that Donald Trump cynically weaponized against all who opposed him.  
-        </p>
-        <p>
-          This base was his success as it gave him not only voting power, but plausible deniability in criminal liability where others could be held accountable. 
-        </p>
-        <p>
-          These are all elements of his successful playbook that was influenced by his early mentor, Roy Cohn, and later directly by the playbooks of global dictators.  
-        </p>
-        <p>
-          A playbook that can now shine light on TRUE CRIMESTOPPERS antidotal remedy to strengthen guardrails around democracy and to prevent crime. 
-        </p>
-        <p>
-          TRUE CRIMESTOPPERS also recognizes that Donald Trump’s base emerged from genuine disaffection with democratic systems that left them out, and that Donald Trump took cynical advantage of. 
-        </p>
-        <p>
-          The world is not good at coming together to solve global problems as proved by climate change. It would therefore benefit from an inclusive solution that would not only safeguard democracy by preventing crime, it would build social capital to achieve greater social and wealth equality. 
-        </p>
-        <p>
-          When it is finally understood that Donald Trump’s motive with his base is woefully insincere, TRUE CRIMESTOPPERS inclusive solution to prevent crime will be the perfect response by all. 
-        </p>
-        <p>
-          It will prove that unified people can prevent crime; and strongly reinforce that unified peoples can also prevent other global problems like climate change and warfare. 
-        </p>
-        <p>
-          TRUE CRIMESTOPPERS can now begin this process...
-        </p>
-        <p>
-          (Read More)
-        </p>
-        <p>
-          
-        </p>
-        <p>
-          
-        </p>
+        <h1 class="article-heading">Our Goals</h1>
+        <div class="article-content">
+          <p>
+            TRUE CRIMESTOPPERS’ first goal is to prevent crime but it recently took on an overarching goal to strengthen guardrails around democracy. 
+          </p>
+          <p>
+            Donald Trump’s victory in the 2024 American election saw his autocratic threat re-emerge which follows extremist trends globally that pose similar threats to democracies. 
+          </p>
+          <p>
+            TRUE CRIMESTOPPERS is non partisan and supports the rule of law underpinning democratic societies to maintain social order and keep people safe. 
+          </p>
+          <p>
+            Donald Trump’s lifetime of moral turpitude has seen him regularly breach the rule of law and defeat all democratic guardrails to be rewarded with the most powerful position in the free world to destroy it for everyone else. 
+          </p>
+          <p>
+            He did not achieve this through political prowess, but by taking advantage of a critical hidden fault in the existing guardrails that has never before been exposed or considered. 
+          </p>
+          <p>
+            TRUE CRIMESTOPPERS exposes this fault in its remedy to Donald Trump’s offending and avoidance of accountability that the existing guardrails could not achieve. 
+          </p>
+          <p>
+            And it also provides the impetus to prevent crime and corruption for the first time in the history of democratic governance. 
+          </p>
+          <p>
+            Ironic, that his playbook to offend and get away with it could now be used to make him accountable and to prevent him and his kind from becoming a demagogue the founding fathers forewarned but could not prevent.  
+          </p>
+          <p>
+            It is also ironic that his final extreme threat to democracy needed to be played out in the presidency in order for TRUE CRIMESTOPPERS’ remedy to be recognized and considered. 
+          </p>
+          <p>
+            Its remedy doesn’t take account of economic, policy or  political reasons Donald Trump was re-elected but focuses instead on a fault in policing and the criminal justice system that is unwitting. 
+          </p>
+          <p>
+            A fault that explains his base and why it supports him in spite of his criminal convictions and obvious moral turpitude. And it explains why that support attracted more momentum with increasing prosecution of his malfeasance. 
+          </p>
+          <p>
+            It explains why Donald Trump became a cult hero, not a cult leader because there was no discernible cult. But there was a legitimate cause in the fault that is one of its consequences. 
+          </p>
+          <p>
+            Whilst it legitimized his base it did not legitimize the fear and intimidatory threat that Donald Trump cynically weaponized against all who opposed him.  
+          </p>
+          <p>
+            This base was his success as it gave him not only voting power, but plausible deniability in criminal liability where others could be held accountable. 
+          </p>
+          <p>
+            These are all elements of his successful playbook that was influenced by his early mentor, Roy Cohn, and later directly by the playbooks of global dictators.  
+          </p>
+          <p>
+            A playbook that can now shine light on TRUE CRIMESTOPPERS antidotal remedy to strengthen guardrails around democracy and to prevent crime. 
+          </p>
+          <p>
+            TRUE CRIMESTOPPERS also recognizes that Donald Trump’s base emerged from genuine disaffection with democratic systems that left them out, and that Donald Trump took cynical advantage of. 
+          </p>
+          <p>
+            The world is not good at coming together to solve global problems as proved by climate change. It would therefore benefit from an inclusive solution that would not only safeguard democracy by preventing crime, it would build social capital to achieve greater social and wealth equality. 
+          </p>
+          <p>
+            When it is finally understood that Donald Trump’s motive with his base is woefully insincere, TRUE CRIMESTOPPERS inclusive solution to prevent crime will be the perfect response by all. 
+          </p>
+          <p>
+            It will prove that unified people can prevent crime; and strongly reinforce that unified peoples can also prevent other global problems like climate change and warfare. 
+          </p>
+          <p>
+            TRUE CRIMESTOPPERS can now begin this process...
+          </p>
+          <p>
+            (Read More)
+          </p>
+        </div>
       </main>
     );
   }
